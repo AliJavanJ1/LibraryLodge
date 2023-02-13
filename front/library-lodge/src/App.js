628abbcfb1f928d4af125967ccf0adda49ec5f1e
amirmoini78 <amirmoini78@gmail.com>
2023-02-13 16:57:24 +0330

    feat(redux): install & provide store

--- front/library-lodge/src/App.js
+++ front/library-lodge/src/App.js
@@ -3,10 +3,10 @@ import {LicenseInfo} from '@mui/x-license-pro';
 LicenseInfo.setLicenseKey('x0jTPl0USVkVZV0SsMjM1kDNyADM5cjM2ETPZJVSQhVRsIDN0YTM6IVREJ1T0b9586ef25c9853decfa7709eee27a1e');
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+        </div>
+    );
 }
 
 export default App;
