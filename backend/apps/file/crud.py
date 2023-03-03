1d3c2e6b96f6936cce1fb6dbec8ee728d40ab2cb
taherkhani <Majid.Taherkhani@digikala.com>
2023-03-03 23:17:19 +0330

    library and file template added

--- backend/apps/file/crud.py
+++ backend/apps/file/crud.py
@@ -10,9 +10,7 @@ templates = Jinja2Templates(directory="templates")
 
 
 def upload_file(db, user_id, file_name, file_upload_info: schemas.FileUpload):
-    id = str(uuid4())
     db_file = models.File(
-        id=id,
         user_id=user_id,
         name=file_name,
         desc = file_upload_info.desc,
@@ -22,7 +20,33 @@ def upload_file(db, user_id, file_name, file_upload_info: schemas.FileUpload):
     db.add(db_file)
     db.commit()
     db.refresh(db_file)
-    return db_file
+    return db_file.id
+
+def create_library(db: Session, library: schemas.CreateLibrary, user_id: int):
+    db_library = models.Library(
+        name=library.name,
+        user_id=user_id,
+        file_template_id=library.file_template_id,
+
+    )
+    db.add(db_library)
+    db.commit()
+    db.refresh(db_library)
+    return db_library.id
+
+def create_file_template(db: Session, fileTemplate: schemas.CreateFileTemplate, user_id: int):
+    db_file_template = models.FileTemplates(
+        name = fileTemplate.name,
+        user_id = user_id,
+        attachments = fileTemplate.attachments,
+        extra_informations = fileTemplate.extra_info
+    )
+    db.add(db_file_template)
+    db.commit()
+    db.refresh(db_file_template)
+    return db_file_template.id
+
+    
 
 def get_file(db: Session, file_id: int, request):
     file = db.query(models.File).filter(models.File.id == file_id).first()
--- backend/apps/file/schemas.py
+++ backend/apps/file/schemas.py
@@ -5,3 +5,12 @@ class FileUpload(BaseModel):
     desc: Optional[str]
     extra_info: dict
     attachments: dict
+
+class CreateLibrary(BaseModel):
+    name: str
+    file_template_id: int
+ 
+class CreateFileTemplate(BaseModel):
+    name: str
+    attachments: list
+    extra_info: list
