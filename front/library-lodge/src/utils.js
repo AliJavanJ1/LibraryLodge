02a0d2789bf3892ea0b1cb25fe56a0574b1b2f20
amirmoini78 <amirmoini78@gmail.com>
2023-03-07 13:15:50 +0330

    feat(ItemList): use data from props to be reusable everywhere.
    fix(ItemList): improve column width so that name takes all available width

--- front/library-lodge/src/utils.js
+++ front/library-lodge/src/utils.js
@@ -10,3 +10,11 @@ export function useLocationItems(){
         return {files: node.files, libs: _.keys(node.libraries)}
     }, [location, tree])
 }
+
+export function useLocationFileTemplate(){
+    const library_details = useSelector(state => state.library_details)
+    const file_templates = useSelector(state => state.file_templates)
+    const location = useSelector(state => state.env.location)
+    const library_detail = location.length > 0 ? library_details[_.last(location)] : null
+    return library_detail ? file_templates[library_detail.file_template] : null
+}
