3d740917b520f9aec0aee727d2681506d37f0886
AliJavanJ1 <aj.ali.javan@gmail.com>
2023-03-09 13:53:42 +0330

    add base router of /api and fix requirements.txt issue and fix some account views APIs

--- backend/main.py
+++ backend/main.py
@@ -1,37 +1,41 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, APIRouter
 import uvicorn
 from apps.account import models as account_models, view as account_view
 from apps.file import models as file_models, view as file_view
 from services.sql_app.database import engine
 from fastapi.staticfiles import StaticFiles
 from starlette_validation_uploadfile import ValidateUploadFileMiddleware
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 account_models.Base.metadata.create_all(bind=engine)
 file_models.Base.metadata.create_all(bind=engine)
 
+router = APIRouter(prefix='/api')
+
 app = FastAPI()
 
-# origins = [
-#     "*"
-# ]
-#
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-app.include_router(account_view.router)
-app.include_router(file_view.router)
-
-
-@app.get("/")
+@router.get("/")
 def healthy_check():
     return 'I am healthy'
+
+router.include_router(account_view.router)
+router.include_router(file_view.router)
+
+app.include_router(router)
 
 
 if __name__ == '__main__':
